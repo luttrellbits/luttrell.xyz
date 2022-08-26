9a0e23f35bb02e136a339908147782ac05515e18
Jason Luttrell <jason@luttrell.xyz>
2022-08-26 17:39:45 -0500

    Add normalize and base layout.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import 'normalize.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style global>
+	:root {
+		--bg0: #2e3440;
+		--bg1: #3b4252;
+		--bg2: #434c5e;
+		--bg3: #4c566a;
+		--fg0: #d8dee9;
+		--fg1: #e5e9f0;
+		--fg2: #eceff4;
+		--acc0: #8fbcbb;
+		--acc1: #88c0d0;
+		--acc3: #81a1c1;
+		--blue: #5e81ac;
+		--red: #bf616a;
+		--orange: #d08770;
+		--yellow: #ebcb8b;
+		--green: #a3be8c;
+		--pink: #b48ead;
+		--black: #060606;
+	}
+
+	body {
+		background-color: var(--bg0);
+		color: var(--fg0);
+	}
+</style>
